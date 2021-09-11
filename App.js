a391abe78f8e3322577d4b7993df48954020b777
Ryan Kang <rkang2009@gmail.com>
2021-09-11 10:44:27 -0600

    added buttons for login and signup

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Login from './Containers/Login/Login';
 import Main from './Containers/Main/Main';
@@ -12,30 +13,32 @@ export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            title: '',
-            headerStyle: {
-              height: '0px'
-            }
-          }}
-        />
-        <Stack.Screen
-          name="Main"
-          component={Main}
-          options={{
-            title: '',
-            headerStyle: {
-              height: '0px'
-            }
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              title: '',
+              headerStyle: {
+                height: '0px'
+              }
+            }}
+          />
+          <Stack.Screen
+            name="Main"
+            component={Main}
+            options={{
+              title: '',
+              headerStyle: {
+                height: '0px'
+              }
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
--- Containers/Login/Login.js
+++ Containers/Login/Login.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
-class Login extends Component {
-render() {
-    return (
-        <View>
-            <Text>This is the login page</Text>
-            <Button
-                title="Go to Main Page"
-                onPress={() =>
-                    this.props.navigation.navigate('Main')
-                }
-            />
-        </View>
-    );
-}
+const styles = StyleSheet.create({
+  buttonStyle: {
+		padding: 24,
+		paddingHorizontal: 50,
+  },
+	buttonContainer: {
+		flexDirection:'row',
+		justifyContent: 'center',
+    alignItems: 'center'
+	},
+	buttonText: {
+		color: 'black'
+	}
+});
+const Login = (props) => {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+	const [buttonSelected, setButtonSelected] = useState(1);
+
+	return (
+		<View>
+			<View style={styles.buttonContainer}>
+				<View style={styles.buttonStyle}>
+					<Button title="Login" type="clear" titleStyle={styles.buttonText}/>
+				</View>
+				<View style={styles.buttonStyle}>
+					<Button title="Sign Up" type="clear" titleStyle={styles.buttonText}/>
+				</View>
+			</View>
+			<Button
+				title="Go to Main Page"
+				onPress={() =>
+					props.navigation.navigate('Main')
+				}
+			/>
+		</View>
+	);
 }
 
 export default Login;
